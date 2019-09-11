17cbf475615d26f518befaab3ab0a8205b158287
Juan Francisco Villanueva <franciscov@lagash.com>
2019-09-11 12:43:31 -0300

    Add project files.

---
+++ ringba-test/Operators.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ringba_test
+{
+    class Operators
+    {
+        public static void PrefixCounter(string text)
+        {
+            Spacer();
+            var mostCommPref = "";
+            var mostCommPrefCount = 0;
+            var mostCommPrefDisplay = "";
+
+            //Starting numbers for i and j are the dec ASCII values for A and a respectively
+            for (int i = 65; i < 91; i++)
+            {
+                for (int j = 97; j < 123; j++)
+                {
+                    var prefixDisplay = ((char) i).ToString() + ((char) j).ToString();
+                    var prefix = prefixDisplay + "[a-z]";
+                    var prefixCount = Regex.Matches(text, prefix).Count;
+
+                    if (prefixCount == mostCommPrefCount)
+                    {
+                        mostCommPref += mostCommPref == "" ? prefix : " " + prefix;
+                    }
+                    else if (prefixCount > mostCommPrefCount)
+                    {
+                        mostCommPref = prefix;
+                        mostCommPrefDisplay = prefixDisplay;
+                        mostCommPrefCount = prefixCount;
+                    }
+                }
+            }
+            Console.WriteLine("Most common prefix(es): {0}. Appearances: {1}", mostCommPrefDisplay, mostCommPrefCount);
+        }
+
+        public static void CapsCounter(string text)
+        {
+            Spacer();
+            var capsCount = Regex.Matches(text, "[A-Z]").Count;
+            Console.WriteLine("Number of capitalized letters: {0}", capsCount);
+        }
+
+        public static void CharCounter(string text)
+        {
+            Spacer();
+            for (int i = 65; i < 91; i++)
+            {
+                string filter = ((char)i) + "|" + ((char) (i + 32));
+                var charCount = Regex.Matches(text, filter).Count;
+                Console.WriteLine("Number of appearances of letter {0}: {1}", ((char)i).ToString(), charCount);
+            }
+        }
+
+        public static void WordCounter(string text)
+        {
+            //A more "correct" strategy would be to add the words to a dictionary and evaluate repetitions
+            //But to save memory I chose this approach instead
+            Spacer();
+            var wordCollection = Regex.Matches(text, @"[\b[A-Z][^A-Z]*]*");
+
+            var mostCommWord = "";
+            var mostCommWordCount = 0;
+
+            for (int i = 0; i < wordCollection.Count; i++)
+            {
+                //This addition to the filter is to keep it from counting the (for example) "Abe" in "Abel"
+                string filter = wordCollection[i].Value + @"\B";
+                var currentWord = Regex.Matches(text,filter);
+
+                if ((currentWord.Count == mostCommWordCount) && (currentWord[0].ToString() != mostCommWord))
+                {
+                    mostCommWord += mostCommWord == "" ? wordCollection[i].Value : ", " + wordCollection[i].Value;
+                }
+                else if (currentWord.Count > mostCommWordCount)
+                {
+                    mostCommWord = wordCollection[i].Value;
+                    mostCommWordCount = currentWord.Count;
+                }
+            }
+
+            Console.WriteLine("Most common word(s): {0}. Appearances: {1}", mostCommWord, mostCommWordCount);
+        }
+
+        private static void Spacer()
+        {
+            Console.WriteLine("--------------");
+            Console.WriteLine();
+            Console.WriteLine("Gathering statistics. Please wait...");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ringba-test/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ringba_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In a real world environment this would be read from a config file
+            var request = System.Net.WebRequest.Create("https://ringba-test-html.s3-us-west-1.amazonaws.com/TestQuestions/output.txt");
+            //In a real world environment this would be read from a config file
+            var savePath = "C:\\Users\\franciscov\\Documents\\output.txt";
+
+
+            Console.WriteLine("Welcome!");
+            //Creating a Spacer() overload for this case seemed unnecesary
+            Console.WriteLine("--------------");
+
+            try
+            {
+                using (Stream stream = request.GetResponse().GetResponseStream())
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Obtaining file. Please wait...");
+
+                    var fStream = new FileStream(savePath, FileMode.Create);
+                    byte[] byteArray = new byte[256];
+                    int count = stream.Read(byteArray, 0, byteArray.Length);
+
+                    while (count > 0)
+                    {
+                        fStream.Write(byteArray, 0, count);
+                        count = stream.Read(byteArray, 0, byteArray.Length);
+                    }
+
+                    fStream.Close();
+                }
+
+                var text = File.ReadAllText(savePath);
+
+                Operators.CharCounter(text);
+                Operators.CapsCounter(text);
+                //Operators.WordCounter(text);
+                Operators.PrefixCounter(text);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error obtaining the file");
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
